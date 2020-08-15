6bd04ce5e1eeea3e7a01d2a2ded2706ef239bf12
Geraldine Craig <gerriemcraig@outlook.com>
2020-08-15 02:26:17 -0400

    Added live forecast

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  return `${day} ${formatHours(timestamp)}`;
+  return `${day} @ ${formatHours(timestamp)}`;
 }
 
 function formatHours(timestamp) {
@@ -52,10 +52,43 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function dispalyForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+      <h3>
+        ${formatHours(forecast.dt * 1000)}
+      </h3>
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+      />
+      <div class="weather-forecast-temperature">
+        <h6>
+          ${Math.round(forecast.main.temp_max)}°
+        <small class="text-muted">
+        ${Math.round(forecast.main.temp_min)}°
+        </small>
+        </h6>
+      </div>
+    </div>
+  `;
+  }
+}
+
 function search(city) {
   let apiKey = "b74337ee99cfbbb35347ab9ecf16bcbc";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(dispalyForecast);
 }
 
 function handleSubmit(event) {
